8bc2e9463e4d65a3c8a3fcfef525be78e0bcf4b2
RL-55 <rickyl55@nycstudents.net>
2025-09-09 08:43:59 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(100)
+turtle.done()
