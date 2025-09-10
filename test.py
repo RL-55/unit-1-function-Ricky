8aa93c8612b65481bc4de244e0efa87e6e90fb1e
RL-55 <rickyl55@nycstudents.net>
2025-09-10 09:20:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(100)
 t.shape('turtle')
-def triangle(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x+25)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x+25)
-triangle(100)
+def square(x,y):
+    for i in range(99):
+        for i in range(4):
+            t.forward(x)
+            t.left(90)
+        t.left(y+4)
+def spiral(x):
+    for i in range(99):
+        for i in range(4):
+            t.forward(i+5)
+            t.left(90)
+        t.left(5)
+spiral(1)
 turtle.done()
