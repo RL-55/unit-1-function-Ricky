336a63a18ebc0e4e9068851f718bd7051d182991
RL-55 <rickyl55@nycstudents.net>
2025-09-11 08:56:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,11 +9,15 @@ def square(x,y):
             t.forward(x)
             t.left(90)
         t.left(y+4)
-def spiral(x):
-    for i in range(99):
-        for i in range(4):
-            t.forward(i+5)
-            t.left(90)
-        t.left(5)
-spiral(1)
+def spiralV1(x):
+        for i in range(120):
+            t.forward(x+i)
+            t.left(89)
+def spiralV2(x):
+    for h in range(80):
+         for i in range(4):
+              t.forward(x+h)
+              t.left(90)
+         t.left(5)
+spiralV2(50)
 turtle.done()
