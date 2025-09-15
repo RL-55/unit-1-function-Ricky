aed6971b4ac912ea6d5781a4a1921db4952ee281
RL-55 <rickyl55@nycstudents.net>
2025-09-15 08:45:24 -0400

    Create new.py

---
+++ new.py
@@ -0,0 +1,11 @@
+name=input("What's your name?")
+print(name)
+def add(x,y):
+    return x*y
+z=add(input("Number One "),input("Number Two "))
+print(z)
+five+int('5')
+print(a+5)
+name+"Mason"
+print(f"His name is {name}")
+ 
