e9c7c99a27413e22e99c967b40b67793061b01af
RL-55 <rickyl55@nycstudents.net>
2025-09-16 08:48:06 -0400

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,27 @@
+name=input("What's your name?")
+print(name)
+def add(x,y):
+    return x*y
+z=add(input("Number One "),input("Number Two "))
+print(z)
+five=int('5')
+print(five+5)
+name="Mason"
+print(f"His name is {name}")
+bill=3.14159
+students=["Cadee","Mason","Andy"]
+students.append("Alina")
+print(students[2])
+for student in students:
+    print(student)
+#Boolean true or false
+x=True
+#evaluations use double==
+if x==True:
+    print()
+else:
+    print()
+y=False
+if y==True:
+elif y==None:
+else y==False:
